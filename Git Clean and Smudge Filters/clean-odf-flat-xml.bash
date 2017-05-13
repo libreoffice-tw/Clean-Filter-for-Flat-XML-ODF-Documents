eec1e0a6e40c11089dccbbb1cfe9f0f9f711dcd0
林博仁 <Buo.Ren.Lin@gmail.com>
2017-05-13 10:09:05 +0800

    將 filter script 移殖到 GNU Bash Shell Script 範本 v1.24.1，順便修正潛在問題
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- Git Clean and Smudge Filters/clean-odf-flat-xml.bash
+++ Git Clean and Smudge Filters/clean-odf-flat-xml.bash
@@ -1,42 +1,58 @@
 #!/usr/bin/env bash
-# 上列為宣告執行 script 程式用的殼程式(shell)的 shebang
+# shellcheck disable=SC2034
+
 # Clean filter for Flat-XML ODF
 # 林博仁 © 2016
 # Ｖ字龍 © 2017
 
-######## File scope variable definitions ########
-# Defensive Bash Programming - not-overridable primitive definitions
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-declare -r PROGRAM_FILENAME="$(basename "$0")"
-declare -r PROGRAM_DIRECTORY="$(realpath --no-symlinks "$(dirname "$0")")"
-declare -r PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
-declare -r PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+# Comments prefixed by BASHDOC: are hints to specific GNU Bash Manual's section:
+# https://www.gnu.org/software/bash/manual/
 
-## Unofficial Bash Script Mode
+## Makes debuggers' life easier - Unofficial Bash Strict Mode
 ## http://redsymbol.net/articles/unofficial-bash-strict-mode/
-# 將未定義的變數的參考視為錯誤
-set -u
+## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
+### Exit prematurely if a command's return value is not 0(with some exceptions), triggers ERR trap if available.
+set -o errexit
 
-# Exit immediately if a pipeline, which may consist of a single simple command, a list, or a compound command returns a non-zero status.  The shell does not exit if the command that fails is part of the command list immediately following a `while' or `until' keyword, part of the test in an `if' statement, part of any command executed in a `&&' or `||' list except the command following the final `&&' or `||', any command in a pipeline but the last, or if the command's return status is being inverted with `!'.  If a compound command other than a subshell returns a non-zero status because a command failed while `-e' was being ignored, the shell does not exit.  A trap on `ERR', if set, is executed before the shell exits.
-set -e
+### Trap on `ERR' is inherited by shell functions, command substitutions, and subshell environment as well
+set -o errtrace
 
-# If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
+### Exit prematurely if an unset variable is expanded, causing parameter expansion failure.
+set -o nounset
+
+### Let the return value of a pipeline be the value of the last (rightmost) command to exit with a non-zero status
 set -o pipefail
 
-######## File scope variable definitions ended ########
+## Non-overridable Primitive Variables
+##
+## BashFAQ/How do I determine the location of my script? I want to read some config files from the same place. - Greg's Wiki
+## http://mywiki.wooledge.org/BashFAQ/028
+RUNTIME_EXECUTABLE_FILENAME="$(basename "${BASH_SOURCE[0]}")"
+declare -r RUNTIME_EXECUTABLE_FILENAME
+declare -r RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "$(realpath --strip "${0}")")"
+declare -r RUNTIME_EXECUTABLE_DIRECTORY
+declare -r RUNTIME_EXECUTABLE_PATH_ABSOLUTE="${RUNTIME_EXECUTABLE_DIRECTORY}/${RUNTIME_EXECUTABLE_FILENAME}"
+declare -r RUNTIME_EXECUTABLE_PATH_RELATIVE="${0}"
+declare -r RUNTIME_COMMAND_BASE="${RUNTIME_COMMAND_BASE:-${0}}"
 
-######## Included files ########
+trap_errexit(){
+	printf "An error occurred and the script is prematurely aborted\n" 1>&2
+	return 0
+}; declare -fr trap_errexit; trap trap_errexit ERR
 
-######## Included files ended ########
+trap_exit(){
+	return 0
+}; declare -fr trap_exit; trap trap_exit EXIT
 
-######## Program ########
-# Defensive Bash Programming - main function, program entry point
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-main() {
+## init function: program entrypoint
+init(){
 	printf "Clean 過濾器：正在移除非必要資訊跟美化 Flat XML ODF 標記代碼……\n" 1>&2
 
 	# Catch the incoming stream to a temp file
-	local temp_file="$(mktemp --tmpdir "$(basename "$PROGRAM_FILENAME")".XXXXXX)"
+	local temp_file
+	temp_file="$(mktemp --tmpdir "$(basename "${RUNTIME_EXECUTABLE_NAME}")".XXXXXX)"
+
 	rm --force "$temp_file"
 	cat >"$temp_file"
 
@@ -64,7 +80,7 @@ main() {
 	xml_delete_node "$temp_file" "/office:document/office:styles/style:default-style"
 
 	# 不追蹤非必要且會變動的 xml:id 屬性
-	xml_transform_node "$PROGRAM_DIRECTORY/$(basename --suffix=.bash "$PROGRAM_FILENAME").remove-xml-id-attributes.xslt" "$temp_file"
+	xml_transform_node "${RUNTIME_EXECUTABLE_DIRECTORY}/$(basename --suffix=.bash "${RUNTIME_EXECUTABLE_NAME}").remove-xml-id-attributes.xslt" "$temp_file"
 
 	xml_format_node "$temp_file"
 
@@ -73,7 +89,13 @@ main() {
 	rm "$temp_file"
 
 	exit 0
-}
+}; declare -fr init
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+declare -r META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.24.1"
+## You may rebase your script to incorporate new features and fixes from the template
 
 xml_delete_node(){
 	local xml_file="$1"
@@ -101,4 +123,4 @@ xml_format_node(){
 	mv --force "$temp_file" "$xml_file"
 }
 
-main
+init "${@}"
--- Git Clean and Smudge Filters/clean-odf-flat-xml.manual-apply.bash
+++ Git Clean and Smudge Filters/clean-odf-flat-xml.manual-apply.bash
@@ -1,57 +1,86 @@
 #!/usr/bin/env bash
-# 上列為宣告執行 script 程式用的殼程式(shell)的 shebang
+# shellcheck disable=SC2034
+
 # Clean filter for Flat-XML ODF wrapper for manual applying
 # 林博仁 © 2016
 # Ｖ字龍 © 2017
 
-######## File scope variable definitions ########
-# Defensive Bash Programming - not-overridable primitive definitions
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-declare -r PROGRAM_FILENAME="$(basename "$0")"
-declare -r PROGRAM_DIRECTORY="$(realpath --no-symlinks "$(dirname "$0")")"
-declare -r PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
-declare -r PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+# Comments prefixed by BASHDOC: are hints to specific GNU Bash Manual's section:
+# https://www.gnu.org/software/bash/manual/
 
-## Unofficial Bash Script Mode
+## Makes debuggers' life easier - Unofficial Bash Strict Mode
 ## http://redsymbol.net/articles/unofficial-bash-strict-mode/
-# 將未定義的變數的參考視為錯誤
-set -u
+## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
+### Exit prematurely if a command's return value is not 0(with some exceptions), triggers ERR trap if available.
+set -o errexit
 
-# Exit immediately if a pipeline, which may consist of a single simple command, a list, or a compound command returns a non-zero status.  The shell does not exit if the command that fails is part of the command list immediately following a `while' or `until' keyword, part of the test in an `if' statement, part of any command executed in a `&&' or `||' list except the command following the final `&&' or `||', any command in a pipeline but the last, or if the command's return status is being inverted with `!'.  If a compound command other than a subshell returns a non-zero status because a command failed while `-e' was being ignored, the shell does not exit.  A trap on `ERR', if set, is executed before the shell exits.
-set -e
+### Trap on `ERR' is inherited by shell functions, command substitutions, and subshell environment as well
+set -o errtrace
 
-# If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
+### Exit prematurely if an unset variable is expanded, causing parameter expansion failure.
+set -o nounset
+
+### Let the return value of a pipeline be the value of the last (rightmost) command to exit with a non-zero status
 set -o pipefail
 
-######## File scope variable definitions ended ########
+## Non-overridable Primitive Variables
+##
+## BashFAQ/How do I determine the location of my script? I want to read some config files from the same place. - Greg's Wiki
+## http://mywiki.wooledge.org/BashFAQ/028
+RUNTIME_EXECUTABLE_FILENAME="$(basename "${BASH_SOURCE[0]}")"
+declare -r RUNTIME_EXECUTABLE_FILENAME
+declare -r RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "$(realpath --strip "${0}")")"
+declare -r RUNTIME_EXECUTABLE_DIRECTORY
+declare -r RUNTIME_EXECUTABLE_PATH_ABSOLUTE="${RUNTIME_EXECUTABLE_DIRECTORY}/${RUNTIME_EXECUTABLE_FILENAME}"
+declare -r RUNTIME_EXECUTABLE_PATH_RELATIVE="${0}"
+declare -r RUNTIME_COMMAND_BASE="${RUNTIME_COMMAND_BASE:-${0}}"
 
-######## Included files ########
+trap_errexit(){
+	printf "An error occurred and the script is prematurely aborted\n" 1>&2
+	return 0
+}; declare -fr trap_errexit; trap trap_errexit ERR
 
-######## Included files ended ########
+trap_exit(){
+	return 0
+}; declare -fr trap_exit; trap trap_exit EXIT
 
-######## Program ########
-# Defensive Bash Programming - main function, program entry point
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-main() {
-	local target_file_path="$PROGRAM_ARGUMENT_ORIGINAL_LIST"
-
-	if [ $PROGRAM_ARGUMENT_ORIGINAL_NUMBER -ne 1 ]; then
-		printf "錯誤：參數數量錯誤。\n" 1>&2
-		printf "資訊：使用方式：$PROGRAM_FILENAME 〈要套用過濾器的檔案〉\n"
-		exit 1
-	else
-		local temp_file_name="$(basename "$target_file_path").new" # Argument list is single filename
-		local temp_file="$PROGRAM_DIRECTORY/$temp_file_name"
-
-		rm --force "$temp_file"
-
-		cat "$PROGRAM_ARGUMENT_ORIGINAL_LIST" | "$PROGRAM_DIRECTORY/$(basename --suffix=.manual-apply.bash "${PROGRAM_FILENAME}").bash" >"$temp_file"
-
-		mv --force "$temp_file" "$PROGRAM_ARGUMENT_ORIGINAL_LIST"
-
-		exit 0
-	fi
-	# Shouldn't be here
+check_runtime_dependencies(){
+	for a_command in cat mktemp mv; do
+		if ! command -v "${a_command}" &>/dev/null; then
+			printf "ERROR: %s command not found.\n" "${a_command}" 1>&2
+			return 1
+		fi
+	done
+	return 0
 }
-main
 
+## init function: program entrypoint
+init(){
+	if ! check_runtime_dependencies; then
+		exit 1
+	fi
+
+	global_temp_file="$(mktemp --tmpdir "${RUNTIME_EXECUTABLE_NAME}.XXXX")"
+	declare -gr global_temp_directory
+
+	if [ "${#}" -ne 1 ]; then
+		printf "錯誤：參數數量錯誤。\n" 1>&2
+		printf "資訊：使用方式：%s 〈要套用過濾器的檔案〉\n" "${RUNTIME_SCRIPT_NAME}" 1>&2
+		exit 1
+	fi
+
+	local target_file="${1}"; shift
+
+	"${RUNTIME_EXECUTABLE_DIRECTORY}"/clean-odf-flat-xml.bash <"${target_file}" >"${global_temp_file}"
+	cat "${global_temp_file}" >"${target_file}"
+
+	exit 0
+}; declare -fr init
+init "${@}"
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+declare -r META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.24.1"
+## You may rebase your script to incorporate new features and fixes from the template
